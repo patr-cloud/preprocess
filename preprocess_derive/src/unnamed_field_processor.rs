314941164b6aea7f48447d3f5df8f9b29c8aae8e
Rakshith Ravi <rakshith.ravi@gmx.com>
2022-10-05 19:23:03 +0530

    Parsing all done. Tokenising left. Enums left

--- preprocess_derive/src/unnamed_field_processor.rs
+++ preprocess_derive/src/unnamed_field_processor.rs
@@ -1,3 +1,5 @@
+use proc_macro2::TokenStream;
+use quote::{format_ident, quote};
 use syn::{Attribute, Type, Visibility};
 
 use super::Preprocessor;
@@ -8,4 +10,16 @@ pub struct UnnamedFieldProcessor {
 	pub vis: Visibility,
 	pub ty: Type,
 	pub preprocessors: Vec<Preprocessor>,
+}
+
+impl UnnamedFieldProcessor {
+	pub fn get_processor_tokens(&self, index: usize) -> TokenStream {
+		let processors = self.preprocessors.iter().map(|processor| {
+			processor.get_processor_token(&format_ident!("field_{}", index))
+		});
+
+		quote! {
+			#(#processors) *
+		}
+	}
 }
